0db3f2a4143045b937e51b365ec64ab00e7d83e7
alextonos-RealEstate <alextonos@gmail.com>
2025-09-01 17:38:35 -0700

    Update pdfdownload.tsx

--- components/pdf/pdfdownload.tsx
+++ components/pdf/pdfdownload.tsx
@@ -2,13 +2,13 @@
 import dynamic from 'next/dynamic'
 import React from 'react'
 
-// This wraps the named export PDFDownloadLink and ensures it only loads on the client.
-const ClientPDFDownloadLink = dynamic(async () => {
+// Loosely typed, client-only wrapper around PDFDownloadLink
+type AnyProps = any
+
+const ClientPDFDownloadLink: React.FC<AnyProps> = dynamic(async () => {
   const mod = await import('@react-pdf/renderer')
-  // Return a component that forwards all props to PDFDownloadLink
-  return function WrappedPDFDownloadLink(props: React.ComponentProps<typeof mod.PDFDownloadLink>) {
-    return <mod.PDFDownloadLink {...props} />
-  }
-}, { ssr: false })
+  const Comp: React.FC<AnyProps> = (props) => <mod.PDFDownloadLink {...props} />
+  return Comp
+}, { ssr: false }) as any
 
 export default ClientPDFDownloadLink
